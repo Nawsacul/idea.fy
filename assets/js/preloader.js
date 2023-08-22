42aaa7dac4e62417a8d7b537f1af82fbd64a29f3
Lucas Queiroz <lucas.queiroz@fda.ufal.br>
2023-08-22 12:37:42 -0300

    teste loader

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -2,5 +2,5 @@ window.addEventListener('load', function () {
     setTimeout(function () {
         const preloader = document.getElementById('preloader');
         preloader.style.display = 'none';
-    }, 500); // Mantém o loader visível por 3 segundos
+    }, 1000); // Mantém o loader visível por 1 segundos
 });
