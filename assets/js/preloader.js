9d308832e0a89a4c0ca149e8fa38d9784f8449b5
Lucas Queiroz <lucas.queiroz@fda.ufal.br>
2023-08-22 12:31:15 -0300

    alterações finais aline tela de loading

---
+++ assets/js/preloader.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function () {
+    setTimeout(function () {
+        const preloader = document.getElementById('preloader');
+        preloader.style.display = 'none';
+    }, 500); // Mantém o loader visível por 3 segundos
+});
