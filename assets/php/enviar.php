f1cd5c5125a408402926b2834b8ba0b97fa51d46
Lucas Queiroz <lucas.queiroz@fda.ufal.br>
2023-09-25 15:00:45 -0300

    Requintada no corpo do email

--- assets/php/enviar.php
+++ assets/php/enviar.php
@@ -5,7 +5,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $termo_documento = isset($_POST["termo_documento"]) ? "Aceito" : "Não aceito";
     $termo_condicoes = isset($_POST["termo_condicoes"]) ? "Aceito" : "Não aceito";
     $termo_privacidade = isset($_POST["termo_privacidade"]) ? "Aceito" : "Não aceito";
-    $termo_avulso = isset($_POST["termo_avulso"]) ? "Aceito" : "Não aceito";
+    $termo_avulso = isset($_POST["termo_avulso"]) ? "Aceito" : 'Não existente';
 
     $pessoaMarca = filter_input(INPUT_POST, "pessoaMarca", FILTER_SANITIZE_STRING);
     if (empty($pessoaMarca)) {
@@ -13,21 +13,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         exit;
     }
 
-    $nomeCompleto = filter_input(INPUT_POST, "nomeCompleto", FILTER_SANITIZE_STRING);
-    $cpf = filter_input(INPUT_POST, "cpf", FILTER_SANITIZE_STRING);
-    $razaoSocial = filter_input(INPUT_POST, "razaoSocial", FILTER_SANITIZE_STRING);
-    $porteEmpresa = filter_input(INPUT_POST, "porteEmpresa", FILTER_SANITIZE_STRING);
-    $cnpj = filter_input(INPUT_POST, "cnpj", FILTER_SANITIZE_STRING);
+    $nomeCompleto = filter_input(INPUT_POST, "nomeCompleto", FILTER_SANITIZE_STRING) ?? '';
+    $cpf = filter_input(INPUT_POST, "cpf", FILTER_SANITIZE_STRING) ?? '';
+
+    $razaoSocial = filter_input(INPUT_POST, "razaoSocial", FILTER_SANITIZE_STRING) ?? '';
+    $porteEmpresa = filter_input(INPUT_POST, "porteEmpresa", FILTER_SANITIZE_STRING) ?? '';
+    $cnpj = filter_input(INPUT_POST, "cnpj", FILTER_SANITIZE_STRING) ?? '';
+
     $cep = filter_input(INPUT_POST, "cep", FILTER_SANITIZE_STRING);
     $endereco = filter_input(INPUT_POST, "endereco", FILTER_SANITIZE_STRING);
     $telefone = filter_input(INPUT_POST, "telefone", FILTER_SANITIZE_STRING);
     $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
-    $dadosPessoais = filter_input(INPUT_POST, "dadosPessoais", FILTER_SANITIZE_STRING);
-    $atividades = filter_input(INPUT_POST, "atividades", FILTER_SANITIZE_STRING);
-    $nomeMarca = filter_input(INPUT_POST, "nomeMarca", FILTER_SANITIZE_STRING);
-    $linguaEstrangeira = filter_input(INPUT_POST, "linguaEstrangeira", FILTER_SANITIZE_STRING);
-    $traducao = filter_input(INPUT_POST, "traducao", FILTER_SANITIZE_STRING);
-    $comoMarcaUtilizada = filter_input(INPUT_POST, "comoMarcaUtilizada", FILTER_SANITIZE_STRING);
+
+    $dadosPessoais = filter_input(INPUT_POST, "dadosPessoais", FILTER_SANITIZE_STRING) ?? '';
+
+    $atividades = filter_input(INPUT_POST, "atividades", FILTER_SANITIZE_STRING) ?? '';
+    $nomeMarca = filter_input(INPUT_POST, "nomeMarca", FILTER_SANITIZE_STRING) ?? '';
+    $linguaEstrangeira = filter_input(INPUT_POST, "linguaEstrangeira", FILTER_SANITIZE_STRING) ?? '';
+    $traducao = filter_input(INPUT_POST, "traducao", FILTER_SANITIZE_STRING) ?? '';
+    $comoMarcaUtilizada = filter_input(INPUT_POST, "comoMarcaUtilizada", FILTER_SANITIZE_STRING) ?? '';
 
     if (!$email) {
         echo "Email inválido.";
@@ -35,26 +39,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     // Construir mensagem de email
-    $mensagem = "Termo de Documento: $termo_documento\n";
-    $mensagem .= "Termo de Condições: $termo_condicoes\n";
-    $mensagem .= "Termo de Privacidade: $termo_privacidade\n";
-    $mensagem .= "Termo Avulso: $termo_avulso\n";
-    $mensagem .= "Pessoa/Marca: $pessoaMarca\n";
-    $mensagem .= "Nome Completo: $nomeCompleto\n";
-    $mensagem .= "CPF: $cpf\n";
-    $mensagem .= "Razão Social: $razaoSocial\n";
-    $mensagem .= "Porte da Empresa: $porteEmpresa\n";
-    $mensagem .= "CNPJ: $cnpj\n";
-    $mensagem .= "CEP: $cep\n";
-    $mensagem .= "Endereço: $endereco\n";
-    $mensagem .= "Telefone: $telefone\n";
-    $mensagem .= "Email: $email\n";
-    $mensagem .= "Dados Pessoais: $dadosPessoais\n";
-    $mensagem .= "Atividades: $atividades\n";
-    $mensagem .= "Nome da Marca: $nomeMarca\n";
-    $mensagem .= "Língua Estrangeira: $linguaEstrangeira\n";
-    $mensagem .= "Tradução: $traducao\n";
-    $mensagem .= "Como a Marca é Utilizada: $comoMarcaUtilizada\n";
+    $mensagem = "<b>Termo de Documento:</b> $termo_documento\n";
+    $mensagem .= "<b>Termo de Condições:</b> $termo_condicoes\n";
+    $mensagem .= "<b>Termo de Privacidade:</b> $termo_privacidade\n";
+    $mensagem .= "<b>Termo Avulso:</b> $termo_avulso\n";
+
+    $mensagem .= "<b>Quem vai registrar a marca?:</b> $pessoaMarca\n";
+
+    if (!empty($nomeCompleto) && !empty($cpf)) {
+        $mensagem .= "<b>Nome Completo:</b> $nomeCompleto\n";
+        $mensagem .= "<b>CPF:</b> $cpf\n";
+    }
+
+    if (!empty($razaoSocial) && !empty($porteEmpresa) && !empty($cnpj)) {
+        $mensagem .= "<b>Razão Social:</b> $razaoSocial\n";
+        $mensagem .= "<b>Porte da Empresa:</b> $porteEmpresa\n";
+        $mensagem .= "<b>CNPJ:</b> $cnpj\n";
+    }
+
+    $mensagem .= "<b>CEP:</b> $cep\n";
+    $mensagem .= "<b>Endereço:</b> $endereco\n";
+    $mensagem .= "<b>Telefone:</b> $telefone\n";
+    $mensagem .= "<b>Email:</b> $email\n";
+    $mensagem .= "<b>Como você prefere que a gente entre em contato?</b> $dadosPessoais\n";
+
+    $mensagem .= "<b>Atividades:</b> $atividades\n";
+
+    $mensagem .= "<b>Nome da Marca:</b> $nomeMarca\n";
+    $mensagem .= "<b>Língua Estrangeira:</b> $linguaEstrangeira\n";
+    $mensagem .= "<b>Tradução:</b> $traducao\n";
+    $mensagem .= "<b>Como a Marca é Utilizada:</b> $comoMarcaUtilizada\n";
 
     // Lidar com o upload de arquivos
     if (isset($_FILES["fileInput"]) && $_FILES["fileInput"]["error"] == UPLOAD_ERR_OK) {
@@ -68,6 +82,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $to = "lucaswan09@gmail.com";
     $subject = "Nova mensagem do formulário";
     $headers = "De: carrinho@ideafy.com.br"; // Substitua pelo email de origem válido no seu domínio
+    // Adicionar cabeçalho de conteúdo HTML
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
     // Verifica se o email foi enviado com sucesso
     if (mail($to, $subject, $mensagem, $headers)) {
