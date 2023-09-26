63304a8ea4b6ebabc292cf6a4bd505d68a7ba605
Lucas Queiroz <lucas.queiroz@fda.ufal.br>
2023-09-26 00:27:48 -0300

    Código php funcional

--- assets/php/enviar.php
+++ assets/php/enviar.php
@@ -7,41 +7,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $termo_privacidade = isset($_POST["termo_privacidade"]) ? "Aceito" : "Não aceito";
     $termo_avulso = isset($_POST["termo_avulso"]) ? "Aceito" : 'Não existente';
 
-    $pessoaMarca = filter_input(INPUT_POST, "pessoaMarca");
-    if (empty($pessoaMarca)) {
-        echo "Tipo de pessoa/marca é obrigatório.";
-        exit;
-    }
+    $pessoaMarca = filter_input(INPUT_POST, "pessoaMarca", FILTER_SANITIZE_STRING);
 
-    $nomeCompleto = filter_input(INPUT_POST, "nomeCompleto") ?? '';
-    $cpf = filter_input(INPUT_POST, "cpf") ?? '';
+    $nomeCompleto = filter_input(INPUT_POST, "nomeCompleto", FILTER_SANITIZE_STRING) ?? '';
+    $cpf = filter_input(INPUT_POST, "cpf", FILTER_SANITIZE_STRING) ?? '';
 
-    $razaoSocial = filter_input(INPUT_POST, "razaoSocial") ?? '';
-    $porteEmpresa = filter_input(INPUT_POST, "porteEmpresa") ?? '';
-    $cnpj = filter_input(INPUT_POST, "cnpj") ?? '';
+    $razaoSocial = filter_input(INPUT_POST, "razaoSocial", FILTER_SANITIZE_STRING) ?? '';
+    $porteEmpresa = filter_input(INPUT_POST, "porteEmpresa", FILTER_SANITIZE_STRING) ?? '';
+    $cnpj = filter_input(INPUT_POST, "cnpj", FILTER_SANITIZE_STRING) ?? '';
 
-    $cep = filter_input(INPUT_POST, "cep");
-    $endereco = filter_input(INPUT_POST, "endereco");
-    $telefone = filter_input(INPUT_POST, "telefone");
+    $cep = filter_input(INPUT_POST, "cep", FILTER_SANITIZE_STRING);
+    $endereco = filter_input(INPUT_POST, "endereco", FILTER_SANITIZE_STRING);
+    $telefone = filter_input(INPUT_POST, "telefone", FILTER_SANITIZE_STRING);
     $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
+    
+    $dadosPessoais = filter_input(INPUT_POST, "dadosPessoais", FILTER_SANITIZE_STRING) ?? '';
 
-    $dadosPessoais = filter_input(INPUT_POST, "dadosPessoais") ?? '';
-
-    $atividades = filter_input(INPUT_POST, "atividades") ?? '';
-    $nomeMarca = filter_input(INPUT_POST, "nomeMarca") ?? '';
-    $linguaEstrangeira = filter_input(INPUT_POST, "linguaEstrangeira") ?? '';
-    $traducao = filter_input(INPUT_POST, "traducao") ?? '';
-    $comoMarcaUtilizada = filter_input(INPUT_POST, "comoMarcaUtilizada") ?? '';
-
-    $planoSelecionado = filter_input(INPUT_POST, "planoSelecionado");
-
-    if (!$email) {
-        echo "Email inválido.";
-        exit;
-    }
+    $atividades = filter_input(INPUT_POST, "atividades", FILTER_SANITIZE_STRING) ?? '';
+    $nomeMarca = filter_input(INPUT_POST, "nomeMarca", FILTER_SANITIZE_STRING) ?? '';
+    $linguaEstrangeira = filter_input(INPUT_POST, "linguaEstrangeira", FILTER_SANITIZE_STRING) ?? '';
+    $traducao = filter_input(INPUT_POST, "traducao", FILTER_SANITIZE_STRING) ?? '';
+    $comoMarcaUtilizada = filter_input(INPUT_POST, "comoMarcaUtilizada", FILTER_SANITIZE_STRING) ?? '';
 
     // Construir mensagem de email
-
     $mensagem = "<b>Termo de Documento:</b> $termo_documento\n";
     $mensagem .= "<b>Termo de Condições:</b> $termo_condicoes\n";
     $mensagem .= "<b>Termo de Privacidade:</b> $termo_privacidade\n";
@@ -49,12 +37,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $mensagem .= "<b>Quem vai registrar a marca?:</b> $pessoaMarca\n";
 
-    if (!empty($nomeCompleto) && !empty($cpf)) {
+    if (empty($nomeCompleto) && empty($cpf)) {
         $mensagem .= "<b>Nome Completo:</b> $nomeCompleto\n";
         $mensagem .= "<b>CPF:</b> $cpf\n";
     }
 
-    if (!empty($razaoSocial) && !empty($porteEmpresa) && !empty($cnpj)) {
+    if (empty($razaoSocial) && empty($porteEmpresa) && empty($cnpj)) {
         $mensagem .= "<b>Razão Social:</b> $razaoSocial\n";
         $mensagem .= "<b>Porte da Empresa:</b> $porteEmpresa\n";
         $mensagem .= "<b>CNPJ:</b> $cnpj\n";
@@ -70,76 +58,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $mensagem .= "<b>Nome da Marca:</b> $nomeMarca\n";
     $mensagem .= "<b>Língua Estrangeira:</b> $linguaEstrangeira\n";
-
-    if (!empty($traducao)) {
-        $mensagem .= "<b>Tradução:</b> $traducao\n";
-    }
+    $mensagem .= "<b>Tradução:</b> $traducao\n";
     $mensagem .= "<b>Como a Marca é Utilizada:</b> $comoMarcaUtilizada\n";
 
     // Lidar com o upload de arquivos
-    if (isset($_FILES['fileInput']) && $_FILES['fileInput']['error'] == UPLOAD_ERR_OK) {
-        // Obter informações do arquivo
-        $fileTmpPath = $_FILES['fileInput']['tmp_name'];
-        $fileName = $_FILES['fileInput']['name'];
-        $fileSize = $_FILES['fileInput']['size'];
-        $fileType = $_FILES['fileInput']['type'];
-
-        // Ler o conteúdo do arquivo
-        $fileContent = file_get_contents($fileTmpPath);
-        $encodedContent = chunk_split(base64_encode($fileContent));
-
-        // Definir o limite
-        $separator = md5(time());
-        $eol = "\r\n";
-
-        // Headers do e-mail
-        $headers = "From: carrinho@ideafy.com.br" . $eol;
-        $headers .= "MIME-Version: 1.0" . $eol;
-        $headers .= "Content-Type: multipart/mixed; boundary=\"" . $separator . "\"" . $eol;
-
-        // Corpo do e-mail
-        $body = "--" . $separator . $eol;
-        $body .= "Content-Type: text/html; charset=\"iso-8859-1\"" . $eol;
-        $body .= "Content-Transfer-Encoding: 7bit" . $eol;
-        $body .= $mensagem . $eol;
-
-        // Anexar arquivo
-        $body .= "--" . $separator . $eol;
-        $body .= "Content-Type: " . $fileType . "; name=\"" . $fileName . "\"" . $eol;
-        $body .= "Content-Transfer-Encoding: base64" . $eol;
-        $body .= "Content-Disposition: attachment" . $eol;
-        $body .= $encodedContent . $eol;
-        $body .= "--" . $separator . "--";
-
-        // Endereço de email para o qual a mensagem será enviada
-        $to = "lucaswan09@gmail.com";
-        $subject = "Novo pedido - Pacote" . $planoSelecionado;
-        $headers = "De: carrinho@ideafy.com.br"; // Substitua pelo email de origem válido no seu domínio
-
-        // Verifica se o email foi enviado com sucesso
-        if (mail($to, $subject, $body, $headers)) {
-            // Redireciona para a página de sucesso
-            header("Location: ../../envio-sucesso.html");
-            exit;
-        } else {
-            // Redireciona para a página de erro
-            header("Location: ../../404.shtml");
-            exit;
-        }
+    if (isset($_FILES["fileInput"]) && $_FILES["fileInput"]["error"] == UPLOAD_ERR_OK) {
+        $nomeArquivo = $_FILES["fileInput"]["name"];
+        $mensagem .= "Arquivo Enviado: $nomeArquivo\n";
     } else {
-        if (isset($_FILES['fileInput']) && $_FILES['fileInput']['error'] == UPLOAD_ERR_NO_FILE) {
-            // Nenhum arquivo foi enviado, mas isso pode ser opcional
-            // Continue processando o resto do formulário
-        } else {
-            // Um erro ocorreu no upload do arquivo
-            if (isset($_FILES['fileInput'])) {
-                echo 'Código de Erro de Upload: ' . $_FILES['fileInput']['error'];
-            } else {
-                echo "Nenhum arquivo foi enviado.";
-            }
-            exit;
-        }
+        $mensagem .= "Nenhum arquivo enviado.\n";
+    }
+
+    // Endereço de email para o qual a mensagem será enviada
+    $to = "lucaswan09@gmail.com";
+    $subject = "Nova mensagem do formulário";
+    $headers = "De: carrinho@ideafy.com.br"; // Substitua pelo email de origem válido no seu domínio
+
+    // Verifica se o email foi enviado com sucesso
+    if (mail($to, $subject, $mensagem, $headers)) {
+        // Redireciona para a página de sucesso
+        header("Location: ../../envio-sucesso.html");
+        exit;
+    } else {
+        // Redireciona para a página de erro
+        header("Location: ../../404.shtml");
+        exit;
     }
 } else {
     echo "Método de requisição inválido.";
 }
+?>
